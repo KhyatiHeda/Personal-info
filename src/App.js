43e2de7e7f89f8264cb3ad0f00c31da9e1cc7ba5
KhyatiHeda <khyati.heda@gmail.com>
2021-05-20 23:27:05 +0530

    commit1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import Main from './main'
 import './App.css';
-import react from 'react';
-function App() {
+import react, { useState } from 'react';
+import axios from 'axios';
+
+const App=()=> {
+
+  
+
+
   return (
     <div className="App">
       <Main />
+      
+             
+          
     </div>
   );
-}
 
+        }
 export default App;
--- src/main.js
+++ src/main.js
@@ -1,15 +1,49 @@
 import './main.css';
-import man from './mot-2.jpg';
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 const Main  = () => {
     
     const[active, setactive]=useState("inactive");
-
+    const [userdata, setuserdata]=useState([]);
+    const [click, setclick]=useState("");
+    const [ActiveUser, SetActiveUser]= useState({
+        image:" " , name: " ", location:" ",loc2: " " ,gender:" "    
+    });
+    
     const HandleClick=()=>
     {
         setactive("active");
     }
+    
+    const HandleClickAgain=()=>
+    {
+        this.setclick("click");
+         SetActiveUser(
+             {
+                 image:userdata[0].picture.thumbnail,
+                 name: userdata[0].name.title+" "+ userdata[0].name.first+" "+ userdata[0].name.last,
+                 location:userdata[0].location.street.number+" "+userdata[0].location.street.name+" "+userdata[0].location.city+" "+userdata[0].location.state+" "+userdata[0].location.country+" "+userdata[0].location.postcode,
+                 loc2:userdata[0].location.timezone.offset+" "+userdata[0].location.timezone.description,
+                 gender:userdata[0].gender
+                 
+             }
+         )
+    }
+    useEffect(()=>{
+        axios.get('https://randomuser.me/api/?inc=gender,name,nat,location,picture,email&results=20')
+     .then((response)=>{
+       
+     setuserdata(response.data.results);
+     })/*.catch((error)=>
+     {
+       setloading=true;
+       console.log(error);
+     })*/
+    },[]);
+    
+    console.log(ActiveUser.gender);
     return ( 
+        
        <nav className="main" style=
        {{
         display:'flex',
@@ -36,107 +70,119 @@ const Main  = () => {
                
            </div>
            
-           <div className="mid-container">
-               <div className="mid-img"><img src={man} alt="Avatar" className="mid-image"></img></div>
-               <div className="mid-text" style=
+            <div className="mid-container" style=
                {{
                 display:'flex',
                 flexDirection:'row',
+                justifyContent:'space-evenly'
+               }} >
+               <div className="mid-img"><img src={ActiveUser.image}  className="mid-image"></img></div> 
+               <div className="mid-text" style=
+               {{
+                display:'flex',
+                flexDirection:'column',
                 justifyContent:'space-around'
                }}>
               
-               <div className="mid-info" >
-               <div className="mid-heading">
-                   <p className="info" id="info1"><u><b>Ms Khyati Heda</b>  </u></p>
-                   <p className="info"><b>Bungalow 4, Bharat Oman Refinery Limited, Agasood</b>  </p>
-                   <p className="info"><b>Bina, Bhopal 470013</b>  </p>
-                   <p className="info" id="gender"> Female </p></div>
-               </div>
-               </div>
                
-           </div>
-
+                   <p className="info" id="info1">{ActiveUser.name}</p>
+                   <p className="info" id="loc">{ActiveUser.location}  </p>
+                   <p className="info" id="loc2">{ActiveUser.loc2}</p>
+                   <p className="info" id="gender"> {ActiveUser.gender} </p></div>
+               </div> 
+              
+               
+           
+            
            <div className="bottom-container">
-               <div className="item-1">
-                   <p className="p1"> Female . NL </p>
-                   <p className="p2"> Ms. Khyati Heda </p>
-                   <p className="p3"> khyati.heda@gmail.com </p>
-               </div>
-               <div className="item-2">
+
+               {userdata.map((user,index)=>
+               { 
+                   return(<div className="card" key={index} onClick={HandleClickAgain} id={click}>
+                   <p className="p1"> {userdata[index].gender+", "+userdata[index].nat} </p>
+                   <p className="p2"> {userdata[index].name.title+" "+userdata[index].name.first +" "+ userdata[index].name.last}  </p>
+                   <p className="p3"> {userdata[index].email} </p>
+                   
+                
+                </div>)
+               })}
+               
+               
+               {/* <div className="item-2" onClick={HandleClickAgain}>
                    <p className="p1"> Male . NL </p>
                    <p className="p2"> Mr. Aritam  </p>
                    <p className="p3"> aritam.asthana@gmail.com </p>
                </div>
-               <div className="item-3">
+               <div className="item-3" onClick={HandleClickAgain}>
                    <p className="p1"> Female . NL </p>
                    <p className="p2"> Ms. Khyati Heda  </p>
                    <p className="p3"> khyati.heda@gmail.com </p>
                </div>
-               <div className="item-4">
+               <div className="item-4" onClick={HandleClickAgain}>
                    <p className="p1"> Male . NL </p>
                    <p className="p2"> Mr. Aritam  </p>
                    <p className="p3"> aritam.asthana@gmail.com </p>
                </div>
-               <div className="item-5">
+               <div className="item-5" onClick={HandleClickAgain}>
                    <p className="p1"> Female . NL </p>
                    <p className="p2"> Ms. Khyati Heda </p>
                    <p className="p3"> khyati.heda@gmail.com </p>
                </div>
-               <div className="item-6">
+               <div className="item-6" onClick={HandleClickAgain}>
                    <p className="p1"> Male . NL </p>
                    <p className="p2"> Mr. Aritam  </p>
                    <p className="p3"> aritam.asthana@gmail.com </p>
                </div>
-               <div className="item-7">
+               <div className="item-7" onClick={HandleClickAgain}>
                    <p className="p1"> Female . NL </p>
                    <p className="p2"> Ms. Khyati Heda </p>
                    <p className="p3"> khyati.heda@gmail.com </p>
                </div>
-               <div className="item-8">
+               <div className="item-8" onClick={HandleClickAgain}>
                    <p className="p1"> Male . NL </p>
                    <p className="p2"> Mr. Aritam  </p>
                    <p className="p3"> aritam.asthana@gmail.com </p>
                </div>
-               <div className="item-9">
+               <div className="item-9" onClick={HandleClickAgain}>
                    <p className="p1"> Female . NL </p>
                    <p className="p2"> Ms. Khyati Heda </p>
                    <p className="p3"> khyati.heda@gmail.com </p>
                </div>
-               <div className="item-10">
+               <div className="item-10" onClick={HandleClickAgain}>
                    <p className="p1"> Male . NL </p>
                    <p className="p2"> Mr. Aritam  </p>
                    <p className="p3"> aritam.asthana@gmail.com </p>
                </div>
-               <div className="item-11">
+               <div className="item-11" onClick={HandleClickAgain}>
                    <p className="p1"> Female . NL </p>
                    <p className="p2"> Ms. Khyati Heda </p>
                    <p className="p3"> khyati.heda@gmail.com </p>
                </div>
-               <div className="item-12">
+               <div className="item-12" onClick={HandleClickAgain}>
                    <p className="p1"> Male . NL </p>
                    <p className="p2"> Mr. Aritam  </p>
                    <p className="p3"> aritam.asthana@gmail.com </p>
                </div>
-               <div className="item-13">
+               <div className="item-13" onClick={HandleClickAgain}>
                    <p className="p1"> Female . NL </p>
                    <p className="p2"> Ms. Khyati Heda </p>
                    <p className="p3"> khyati.heda@gmail.com </p>
                </div>
-               <div className="item-14">
+               <div className="item-14" onClick={HandleClickAgain}>
                    <p className="p1"> Male . NL </p>
                    <p className="p2"> Mr. Aritam  </p>
                    <p className="p3"> aritam.asthana@gmail.com </p>
                </div>
-               <div className="item-15">
+               <div className="item-15" onClick={HandleClickAgain}>
                    <p className="p1"> Female . NL </p>
                    <p className="p2"> Ms. Khyati Heda </p>
                    <p className="p3"> khyati.heda@gmail.com </p>
                </div>
-               <div className="item-16">
+               <div className="item-16" onClick={HandleClickAgain}>
                    <p className="p1"> Male . NL </p>
                    <p className="p2"> Mr. Aritam  </p>
                    <p className="p3"> aritam.asthana@gmail.com </p>
-               </div>
+               </div> */}
            </div>
        </nav>
      );
